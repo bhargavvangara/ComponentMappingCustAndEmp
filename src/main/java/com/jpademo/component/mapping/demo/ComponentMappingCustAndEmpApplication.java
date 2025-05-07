0c9d8799a73ec89894cc991223d7777f77829c0e
bhargavvangara <bhargavvangara5@gmail.com>
2025-05-07 23:32:37 +0530

    initial repo

---
+++ src/main/java/com/jpademo/component/mapping/demo/ComponentMappingCustAndEmpApplication.java
@@ -0,0 +1,13 @@
+package com.jpademo.component.mapping.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComponentMappingCustAndEmpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComponentMappingCustAndEmpApplication.class, args);
+	}
+
+}
